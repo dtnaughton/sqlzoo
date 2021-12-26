241aeb54bfa61b3889981402b452bd840d8a260f
dtnaughton <77862637+dtnaughton@users.noreply.github.com>
2021-12-25 19:32:23 -0500

    updated select-world

--- select-world.sql
+++ select-world.sql
@@ -47,4 +47,38 @@ SELECT name, population, area FROM world
 SELECT name, population, area FROM world
     WHERE population > 250E6 XOR area > 3E6
     
+-- 9.
+-- Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'. Use the ROUND function to show the values to two decimal places.
+
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000,2) FROM world
+    WHERE continent = 'South America'
+
+-- 10.
+-- Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+-- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+
+SELECT name, ROUND(GDP/population,-3) FROM world
+    WHERE GDP > 1000000000000
+
+-- 11.
+-- Greece has capital Athens.
+
+-- Each of the strings 'Greece', and 'Athens' has 6 characters.
+
+-- Show the name and capital where the name and the capital have the same number of characters.
+
+SELECT name, capital FROM world
+    WHERE LENGTH(name) = LENGTH(capital)
+
+-- 12.
+-- The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+
+-- Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+
+SELECT name, capital FROM world
+    WHERE LEFT(name, 1) = LEFT(capital, 1)
+    AND name <> capital
 
