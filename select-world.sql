fbda66c1da989168864d1908258e73c26fe5f676
dtnaughton92 <77862637+dtnaughton92@users.noreply.github.com>
2021-12-28 12:46:38 -0500

    updated select-world and added select-nobel

--- select-world.sql
+++ select-world.sql
@@ -82,3 +82,14 @@ SELECT name, capital FROM world
     WHERE LEFT(name, 1) = LEFT(capital, 1)
     AND name <> capital
 
+-- 13.
+-- Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+
+SELECT name FROM world
+WHERE
+    (name LIKE '%a%') AND
+    (name LIKE '%e%') AND
+    (name LIKE '%i%') AND
+    (name LIKE '%o%') AND
+    (name LIKE '%u%') AND
+    (name NOT LIKE '% %')
