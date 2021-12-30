9df382ce23d4fdf830ba3065fecb95ffa86df878
dtnaughton92 <77862637+dtnaughton92@users.noreply.github.com>
2021-12-30 14:22:22 -0500

    update to select-nobel and added select-within-select

--- select-nobel.sql
+++ select-nobel.sql
@@ -69,3 +69,33 @@ SELECT * FROM nobel
 WHERE (subject = 'Medicine' AND yr < 1910)
 OR (subject = 'Literature' AND yr >= 2004)
 
+-- 11.
+-- Find all details of the prize won by PETER GRÜNBERG
+
+SELECT * FROM nobel
+WHERE winner = 'PETER GRÜNBERG'
+
+-- 12.
+-- Find all details of the prize won by EUGENE O'NEILL
+
+SELECT * FROM nobel
+WHERE winner = 'EUGENE O''NEILL'
+
+-- 13.
+-- Knights in order
+
+-- List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+SELECT winner, yr, subject FROM nobel
+WHERE winner LIKE 'Sir%'
+ORDER BY yr DESC, winner
+
+-- 14.
+-- The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
+
+-- Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject FROM nobel
+WHERE yr = 1984
+ORDER BY subject IN('Chemistry', 'Physics'), subject, winner
+
